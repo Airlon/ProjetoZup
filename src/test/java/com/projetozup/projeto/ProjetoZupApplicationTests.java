3e471ca8592e20a857d11e9f1c54af4852436afd
Airlon Servulo <airlon.servulo@outlook.com>
2021-04-15 19:47:13 -0300

    Projeto Criado

---
+++ src/test/java/com/projetozup/projeto/ProjetoZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetozup.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
